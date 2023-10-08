00a96018ade65bae2e8da19dc07f903407beca09
vacuumbeef <51813411+vacuumbeef@users.noreply.github.com>
2023-10-08 13:34:34 +0300

    Update vayu-postboot.sh - delete tap-to-wake

--- usr/bin/vayu-postboot.sh
+++ usr/bin/vayu-postboot.sh
@@ -4,6 +4,3 @@ if [ -z "${LAUNCHED_BY_SYSTEMD}" ]; then
     echo "This script is automatically executed at boot by systemd. Quiting.."
     exit 1
 fi
-
-# Enable DT2W
-echo 1 > /sys/touchpanel/double_tap
